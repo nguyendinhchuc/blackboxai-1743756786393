a4a04a8669c50c009d1411427ed27b08ac00f37b
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:54:54 +0700

    Add blackboxai-1743441286143/blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Product.java 1743756883140

---
+++ blackboxai-1743441286143/blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Product.java
@@ -0,0 +1,34 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private Integer stockQuantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ProductImage> images = new HashSet<>();
+}
