dbf01f15cd039eee457e75757b92c434fd9e4a83
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:53:51 +0700

    Add blackboxai-1743441286143/src/main/java/com/ecommerce/api/repository/TenantRepository.java 1743756819845

---
+++ blackboxai-1743441286143/src/main/java/com/ecommerce/api/repository/TenantRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.api.repository;
+
+import com.ecommerce.api.model.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+    Optional<Tenant> findByDomain(String domain);
+}
