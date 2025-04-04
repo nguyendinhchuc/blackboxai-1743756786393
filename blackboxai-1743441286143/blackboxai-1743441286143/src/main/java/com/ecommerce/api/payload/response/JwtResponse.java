e9c535df9b8aa8239dfb2ed18a6fa11f532ab200
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:54:37 +0700

    Add blackboxai-1743441286143/blackboxai-1743441286143/src/main/java/com/ecommerce/api/payload/response/JwtResponse.java 1743756865926

---
+++ blackboxai-1743441286143/blackboxai-1743441286143/src/main/java/com/ecommerce/api/payload/response/JwtResponse.java
@@ -0,0 +1,44 @@
+package com.ecommerce.api.payload.response;
+
+import java.util.List;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String token, Long id, String username, String email, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
