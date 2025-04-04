4f6185ce51cc4952eef3bf192c3acfef9140d0b3
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:55:23 +0700

    Add blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Payment.java 1743756911875

---
+++ blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Payment.java
@@ -0,0 +1,28 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private BigDecimal amount;
+    private String paymentMethod;
+    private String transactionId;
+    private String status;
+    private LocalDateTime paymentDate;
+}
