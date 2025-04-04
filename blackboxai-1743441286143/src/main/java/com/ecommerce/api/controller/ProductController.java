c6dacaae563e030ad204c2e01bb4c363a1e2a981
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:55:37 +0700

    Add blackboxai-1743441286143/src/main/java/com/ecommerce/api/controller/ProductController.java 1743756925471

---
+++ blackboxai-1743441286143/src/main/java/com/ecommerce/api/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.ecommerce.api.controller;
+
+import com.ecommerce.api.model.Product;
+import com.ecommerce.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<Product> getProductsByCategory(@PathVariable Long categoryId) {
+        return productService.getProductsByCategory(categoryId);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        return productService.updateProduct(id, productDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+}
