6c1acccfd3fdb272ca3aa56009dbfc03cee04d01
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:55:18 +0700

    Add blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/ProductImage.java 1743756906905

---
+++ blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/ProductImage.java
@@ -0,0 +1,19 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "product_images")
+@Data
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
