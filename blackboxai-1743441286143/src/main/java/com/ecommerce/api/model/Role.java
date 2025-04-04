4bd6d17cab5b256df05309c5f2370f012cc150b2
nguyendinhchuc <nguyendinhchuc996@gmail.com>
2025-04-04 15:55:00 +0700

    Add blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Role.java 1743756888886

---
+++ blackboxai-1743441286143/src/main/java/com/ecommerce/api/model/Role.java
@@ -0,0 +1,25 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleEnum name;
+
+    public Role() {}
+
+    public Role(RoleEnum name) {
+        this.name = name;
+    }
+}
